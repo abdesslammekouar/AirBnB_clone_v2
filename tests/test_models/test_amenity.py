c85336830af94073e4ba02ce6ab57dc7987d2d6a
CloudyHeaveen <abirarahou2002@gmail.com>
2023-09-19 14:34:03 -0700

    ALL TASKS

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,18 +2,21 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
+import os
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """ amenity test class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initialize the test class """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """ Testing name type """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        expected_type = str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        self.assertEqual(type(new.name), expected_type)
+
