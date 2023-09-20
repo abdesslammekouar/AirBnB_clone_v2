c56482d706e5cc80ac0fec064f691fc887cb8f75
CloudyHeaveen <abirarahou2002@gmail.com>
2023-09-20 08:34:41 -0700

    ALL TASKS

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module instantiates an instance of the Storage will be used"""
-
+from models.engine.file_storage import FileStorage
 from os import getenv
 
 storage_type = getenv('HBNB_TYPE_STORAGE')
@@ -9,8 +9,6 @@ if storage_type == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
 storage.reload()
-
